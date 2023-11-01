146bf72a01b27cdacea5556e5807b9fb89f9d8a0
Suppanut Nguansamang <nutcom3@gmail.com>
2023-11-02 02:03:11 +0700

    for tablet size all

--- components/basketball/player/index.tsx
+++ components/basketball/player/index.tsx
@@ -2,9 +2,9 @@ import styles from './player.module.css'
 
 export default function Athlets() {
   return (
-    <div className={styles.athlets}>
+    <div className={styles.player}>
       <div className={styles.headArticle}>
-        <div className={styles.athletsText}>PLAYERS</div>
+        <div className={styles.playerText}>PLAYERS</div>
       </div>
     </div>
   )
